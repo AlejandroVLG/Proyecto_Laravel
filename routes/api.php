90f9c3c80d78fe8913ba755ff56b38151fb7353d
Alexlg <Alejandrolg86@gmail.com>
2022-07-26 09:46:51 +0200

    feat: route / created

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {return ['Bienvenido a mi api'];});
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
